3fe2fc6767afebb1c43cf4491d706376eb93e8b4
young-sinf <wjdtp93@gmail.com>
2022-10-06 15:07:24 +0900

    Reset project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { RecoilRoot } from 'recoil';
 import './App.css';
-import CharacterCounter from './components/CharacterCounter';
 
 function App() {
   return (
     <RecoilRoot>
-      <CharacterCounter />
+      <></>
     </RecoilRoot>
   );
 }
